55dc0b865241cefd317297de03b8086776873ac6
Vaishnavi Shivde <76559402+vaishnavishivde98@users.noreply.github.com>
2024-05-07 20:40:07 +0530

    Create Jenkins_installation.sh

---
+++ Jenkins_installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Installation of Java
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+
+#Jenkins Installation [ Long Term Support release ]
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
+sudo apt-get update
+sudo apt-get install jenkins -y 
+
+#Start and Enable jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
